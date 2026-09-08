20d7ecb2bd929fbe5721e0524cf1b9badf4ab079
YoussefAtefHenin <youssef.atef.fci@gmail.com>
2026-09-08 19:44:38 +0300

    first commit

---
+++ lib/banking_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class BankingApp extends StatelessWidget {
+  const BankingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      // home:  اول اسكرين ,
+    );
+  }
+}
